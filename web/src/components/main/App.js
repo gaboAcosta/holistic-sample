78221082d91482944b396391034d83b8b35ff30e
Gabo Acosta <gabo.acosta624@gmail.com>
2017-09-05 17:05:39 -0500

    Removing mobx devtools until needed

--- web/src/components/main/App.js
+++ web/src/components/main/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import TopBar from './TopBar'
 import Footer from './Footer' 
 import Body from './Body'
-import DevTools from 'mobx-react-devtools'
 
 export default class App extends Component {
 
@@ -10,7 +9,6 @@ export default class App extends Component {
         return (
             // the main app content for e2e tests
             <div id="holistic">
-                <DevTools />
                 <TopBar />
                 <Body/>
                 <Footer/>
